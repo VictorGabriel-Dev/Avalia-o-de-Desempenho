6176744ea98571a6d1e7d27c68f73b0c68dc5be2
Kevin-Barbosa-Dev <kevin6252@outlook.com>
2025-05-28 20:11:51 -0300

    ...

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    int[] vetor = {7, 4, 5, 8, 3, 6, 10, 9};
+
+    public int[] ordenarEmOrdemCrescente() {
+        for (int i = 0; i < vetor.length - 2; i++) {
+            for (int j = 0; j < vetor.length - 2 - i; j++) {
+                if (vetor[j] > vetor[j + 1]) {
+                    int temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                }
+            }
+
+        }
+        return vetor;
+    }
+
+    @Override
+    public String toString() {
+        return "BubbleSort{" +
+                "vetor=" + Arrays.toString(vetor) +
+                '}';
+    }
+}
+
