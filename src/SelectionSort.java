84bdb02a48e618f0dce0f21fa35df08f35a228ee
Victor_Alves <164104733+VictorGabriel-Dev@users.noreply.github.com>
2025-05-28 21:14:30 -0300

    Update SelectionSort.java

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,15 @@
 public class SelectionSort {
+    static void selectionSort(int [] valor){
+        int n = valor.length;
+
+        for(int i=0; i< n -1; i++){
+            int menor_idx = i;
+
+            for(int j= i+1; j< n; j++){
+                if(valor[j] < valor[menor_idx]){
+                    menor_idx = j;
+                }
+            }
+        }
+    }
 }
