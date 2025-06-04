115b5c4d25a273c175d1c71422ab7a40e7e2c2cc
Kevin-Barbosa-Dev <kevin6252@outlook.com>
2025-06-04 20:28:11 -0300

    ...

--- src/controller/AlgoritmosController.java
+++ src/controller/AlgoritmosController.java
@@ -3,6 +3,9 @@ package controller;
 import model.*;
 import view.AlgoritmosView;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static java.lang.System.nanoTime;
 
 public class AlgoritmosController {
@@ -16,27 +19,51 @@ public class AlgoritmosController {
             switch (opcao) {
                 case 1:
                     algoritmo = new BubbleSort();
+                    executarAlgoritmo(algoritmo, numeros);
                     break;
                 case 2:
                     algoritmo = new SelectionSort();
+                    executarAlgoritmo(algoritmo, numeros);
                     break;
                 case 3:
                     algoritmo = new HeapSort();
+                    executarAlgoritmo(algoritmo, numeros);
+                    break;
+                case 4:
+                    executarTodos(numeros);
                     break;
                 case 0:
                     view.sair();
                     return;
                 default:
                     view.invalido();
-                    continue;
             }
-
-            long inicio = nanoTime();
-            int[] resultado = algoritmo.ordenar(numeros.clone());
-            long fim = nanoTime();
-
-            long duracao = fim - inicio;
-            view.mostrarResultado(algoritmo.getClass().getSimpleName(), resultado, duracao);
         }
+    }
+
+    private void executarAlgoritmo(Ordenador algoritmo, int[] numeros) {
+        long inicio = nanoTime();
+        int[] resultado = algoritmo.ordenar(numeros.clone());
+        long fim = nanoTime();
+        long duracao = fim - inicio;
+
+        view.mostrarResultado(algoritmo.getClass().getSimpleName(), resultado, duracao);
+    }
+
+    private void executarTodos(int[] numeros) {
+        Map<String, Long> tempos = new LinkedHashMap<>(); // Mantém a ordem de inserção
+
+        tempos.put("BubbleSort", medirTempo(new BubbleSort(), numeros));
+        tempos.put("SelectionSort", medirTempo(new SelectionSort(), numeros));
+        tempos.put("HeapSort", medirTempo(new HeapSort(), numeros));
+
+        view.mostrarTabelaComparativa(tempos);
+    }
+
+    private long medirTempo(Ordenador algoritmo, int[] numeros) {
+        long inicio = nanoTime();
+        algoritmo.ordenar(numeros.clone());
+        long fim = nanoTime();
+        return fim - inicio;
     }
 }
--- src/view/AlgoritmosView.java
+++ src/view/AlgoritmosView.java
@@ -1,34 +1,61 @@
 package view;
 
 import java.util.Scanner;
+import java.util.Map;
 
 public class AlgoritmosView {
     Scanner scanner = new Scanner(System.in);
-    public int menu(){
+
+    public int menu() {
         System.out.println("\n--- Escolha o algoritmo de ordenação ---");
         System.out.println("1 - Bubble Sort");
         System.out.println("2 - Selection Sort");
         System.out.println("3 - Heap Sort");
+        System.out.println("4 - Executar todos e mostrar tabela");
         System.out.println("0 - Sair");
 
         System.out.print("Opção: ");
         return scanner.nextInt();
     }
 
-    public void mostrarResultado(String nomeAlgoritmo, int[] resultado, long duracao) {
-        System.out.print(nomeAlgoritmo + ": ");
+    public void mostrarResultado(String nomeAlgoritmo, int[] resultado, long duracaoNano) {
+        System.out.print(nomeAlgoritmo + " Resultado: ");
         for (int num : resultado) {
             System.out.print(num + " ");
         }
-        System.out.println("\nTempo de execução: " + duracao + " ns");
-        System.out.println();
+
+        double duracaoMili = duracaoNano / 1_000_000.0;
+        double duracaoSeg = duracaoNano / 1_000_000_000.0;
+
+        System.out.println("\n----------------------------------------------");
+        System.out.println("Algoritmo       | Nanosegundos | Milissegundos | Segundos");
+        System.out.println("-----------------------------------------------------------");
+        System.out.printf("%-15s| %-13d| %-15.3f| %-8.6f%n",
+                nomeAlgoritmo, duracaoNano, duracaoMili, duracaoSeg);
+        System.out.println("-----------------------------------------------------------\n");
+    }
+
+    public void mostrarTabelaComparativa(Map<String, Long> tempos) {
+        System.out.println("\n--- Tabela Comparativa de Desempenho ---");
+        System.out.println("Algoritmo       | Nanosegundos | Milissegundos | Segundos");
+        System.out.println("-----------------------------------------------------------");
+
+        for (Map.Entry<String, Long> entry : tempos.entrySet()) {
+            long nano = entry.getValue();
+            double mili = nano / 1_000_000.0;
+            double seg = nano / 1_000_000_000.0;
+            System.out.printf("%-15s| %-13d| %-15.3f| %-8.6f%n",
+                    entry.getKey(), nano, mili, seg);
+        }
+
+        System.out.println("-----------------------------------------------------------\n");
     }
 
     public void invalido() {
         System.out.println("Opção inválida.");
     }
-    public  void sair(){
+
+    public void sair() {
         System.out.println("Encerrando...");
     }
-
 }
